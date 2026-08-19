0c7a14022b065676c98edc1b7fa2a848f276fd9a
4oON <leoxieni@gmail.com>
2026-08-19 09:15:06 +0800

    feat(watch): add ZenFitTimerWidget extension target
    - New WidgetKit extension for watchOS lock screen / Smart Stack
    - Shows rest timer countdown on watch face
    - Target created via Xcode, pending code implementation

---
+++ frontend/ios/App/ZenFitTimerWidget/ZenFitTimerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ZenFitTimerWidgetBundle.swift
+//  ZenFitTimerWidget
+//
+//  Created by SKL on 19/8/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ZenFitTimerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ZenFitTimerWidget()
+    }
+}
